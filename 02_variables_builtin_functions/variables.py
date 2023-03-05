adfcbdb1a32f0fead60614186967eb40fb2e0ca0
James Stopford <jamesstopford@gmail.com>
2023-03-05 13:09:57 -0500

    Day 2

---
+++ 02_variables_builtin_functions/variables.py
@@ -0,0 +1,11 @@
+# Day 2 - 30 days of python
+
+first_name = 'James'
+last_name = 'Stopford'
+full_name = first_name + ' ' + last_name
+
+idea1, idea2, idea3 = 'I do not like this pattern', 'This is not cool', 'What?'
+
+
+print('Hello ' + full_name)
+print(idea2)
