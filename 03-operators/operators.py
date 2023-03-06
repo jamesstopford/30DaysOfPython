051a70d430b18c2155c0dbe4476f7dbbaaa5d66c
James Stopford <jamesstopford@gmail.com>
2023-03-06 07:36:23 -0500

    Day 3

---
+++ 03-operators/operators.py
@@ -0,0 +1,16 @@
+age = 38
+height = 6.33
+
+fav = input('Favorite color')
+
+print(fav)
+
+base = float(input('triangle base: '))
+height = float(input('triangle height: '))
+triangle_area = base * height * .5
+
+print('Area: ' + str(triangle_area))
+
+daTruth = len('python') is len('dragon')
+
+print('Da truth: ' + str(daTruth))
