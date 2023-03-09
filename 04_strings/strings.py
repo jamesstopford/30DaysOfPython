a3bcda8872d3f08e5781a3e3b87a131f1065f23a
James Stopford <jamesstopford@gmail.com>
2023-03-09 12:23:54 -0500

    Day 4 and 5

---
+++ 04_strings/strings.py
@@ -0,0 +1,6 @@
+language = 'Python'
+pto = language[0:6:2] #What the heck is going on here...
+print(pto)
+
+
+a = "thirty" + "days"
---
+++ 05_lists/lists.py
@@ -0,0 +1,44 @@
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+print('Number of web technologies:', len(web_techs))
+
+
+# List Packing craziness.
+
+lst = ['item','item2','item3', 'item4', 'item5']
+first_item, second_item, third_item, *rest = lst
+print(first_item)     # item1
+print(second_item)    # item2
+print(third_item)     # item3
+print(rest)           # ['item4', 'item5']
+
+# First Example
+fruits = ['banana', 'orange', 'mango', 'lemon','lime','apple']
+first_fruit, second_fruit, third_fruit, *rest = lst
+print(first_fruit)     # banana
+print(second_fruit)    # orange
+print(third_fruit)     # mango
+print(rest)           # ['lemon','lime','apple']
+# Second Example about unpacking list
+first, second, third,*rest, tenth = [1,2,3,4,5,6,7,8,9,10]
+print(first)          # 1
+print(second)         # 2
+print(third)          # 3
+print(rest)           # [4,5,6,7,8,9]
+print(tenth)          # 10
+# Third Example about unpacking list
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+gr, fr, bg, sw, *scandic, es = countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.pop(0)) # Pop an item - drop from list - pop returns the deleted item. del does not
+does_exist = 'banana' in fruits
+print(does_exist)
+does_exist = 'lime' in fruits
+print(does_exist)
