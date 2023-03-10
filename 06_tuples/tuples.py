9d4d006faa1fb754e5f4e9a369377ec59c005ab6
James Stopford <jamesstopford@gmail.com>
2023-03-10 12:08:01 -0500

    DAy 6 - Tuples

---
+++ 06_tuples/tuples.py
@@ -0,0 +1,9 @@
+fruits = ('banana', 'orange', 'mango')
+first_fruit, second_fruit, third_fruit = fruits
+print(first_fruit)     # banana
+print(second_fruit)    # orange
+print(third_fruit)     # mango
+
+print( 'banana' in fruits) # True
+
+print( 'bonano' in fruits) # True
