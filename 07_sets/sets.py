0c74f9b07240fb2d208b5efa3e08008f2ac1b19d
James Stopford <jamesstopford@gmail.com>
2023-03-11 09:57:45 -0500

    DAy 7 - sets

---
+++ 07_sets/sets.py
@@ -0,0 +1,12 @@
+st = {'item1', 'item2', 'item3', 'item4'}
+print("Does set st contain item3? ", 'item3' in st) # Does set st contain item3? True
+
+
+# set {}
+# list []
+# tuple ()
+
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+removed_item = fruits.pop() #pops and returns a random item in set
+print(removed_item)
