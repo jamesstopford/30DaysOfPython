fdf3d3259db72c55f53ed5b8ef665e165505966d
James Stopford <jamesstopford@gmail.com>
2023-04-02 11:58:05 -0400

    Day 18 - regex

---
+++ 18_regex/regex_day.py
@@ -0,0 +1,21 @@
+import re
+
+txt = 'I love to teach python and javaScript'
+# It returns an object with span, and match
+match = re.match('I love to teach', txt, re.I)
+print(match)  # <re.Match object; span=(0, 15), match='I love to teach'>
+# We can get the starting and ending position of the match as tuple using span
+span = match.span()
+print(span)     # (0, 15)
+# Lets find the start and stop position from the span
+start, end = span
+print(start, end)  # 0, 15
+substring = txt[start:end]
+print(substring)       # I love to teach
+
+
+txt = '''I am teacher and  I love teaching.
+There is nothing as rewarding as educating and empowering people.
+I found teaching more interesting than any other jobs.
+Does this motivate you to be a teacher?'''
+print(re.split('\n', txt)) # splitting using \n - end of line symbol
